6b07815dba5cfbd1db2117579e6b510119ec33e9
Pedro Passos <102426718+pedr0passos@users.noreply.github.com>
2022-11-15 10:28:18 -0300

    Update Novo.java

--- lista05/q13/Novo.java
+++ lista05/q13/Novo.java
@@ -5,9 +5,13 @@ public class Novo extends Imovel{
     private double adicional;
 
     public Novo () {
-        
+        this("Nenhum", 0, 0);
     }
 
+    public Novo (String e, double p, double a ) {
+        super(e,p);
+        this.adicional = a;
+    }
     public double getAdicional () {
         return this.adicional;
     }
